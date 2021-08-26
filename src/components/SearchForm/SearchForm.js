4e0db25aa62bbe9074f5c0978cfc39d8b4da00b8
Kirill Fedotov <knprcta@yandex.ru>
2021-08-26 20:10:09 +0300

    fix: выполнить некоторые рекомендации ревьюера

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -7,7 +7,7 @@ function SearchForm() {
   return (
     <form className="search">
       <div className="search__container">
-        <input className="search__input" type="text" placeholder="Фильм" />
+        <input className="search__input" type="text" placeholder="Фильм" required/>
         <SearchBtn />
       </div>
       <div className="search__checkbox-wrapper">
